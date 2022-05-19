508b77714730fbfb275bd1cfdce01f80569dd516
Halil İbrahim Kaya <halil.kaya@naylalabs.com>
2022-05-20 00:28:33 +0300

    Remove unnecessary codes.

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ app = Flask(__name__,
             template_folder='web/templates')
 
 video = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier()
-face_cascade.load(cv2.samples.findFile("./haarcascade_frontalface_alt.xml"))
 
 @app.route('/',methods = ['GET','POST'])
 def index():
@@ -49,21 +47,8 @@ def gen(video):
         success, image = video.read()
         #todo: put the img to ml model
 
-        frame_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        frame_gray = cv2.equalizeHist(frame_gray)
-
-        faces = face_cascade.detectMultiScale(frame_gray)
-
-        for (x, y, w, h) in faces:
-            center = (x + w//2, y + h//2)
-            cv2.putText(image, "X: " + str(center[0]) + " Y: " + str(center[1]), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
-            image = cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-            faceROI = frame_gray[y:y+h, x:x+w]
         ret, jpeg = cv2.imencode('.jpg', image)
-
         frame = jpeg.tobytes()
-        
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
